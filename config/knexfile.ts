ad8eb492fd1450fb74b6058fa0e76580ce440183
María Umaña <aux_uc@outlook.com>
2021-12-19 15:58:24 -0600

    feature: configurations

--- config/knexfile.ts
+++ config/knexfile.ts
@@ -4,7 +4,7 @@ const dbSecrets = {
     host: process.env.HOST || "127.0.0.1",
     port: process.env.PORT || "3306",
     user: "root",
-    password: process.env.PASSWORD || "",
+    password: process.env.PASSWORD || "yourpassword",
     database: process.env.DATABASE || "cats",
   },
 };
